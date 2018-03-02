e3e933d651a4b300a30bbcb4c1b8ab20fbbf640f
Jorge Rodríguez García <jorger09@ucm.es>
2018-03-02 11:36:55 +0100

    ficheros sobrantes eliminados, hay 9 errores de compilacion por inclusiones

--- Ficheros/GunInputComponent.h
+++ Ficheros/GunInputComponent.h
@@ -2,13 +2,14 @@
 #include "InputComponent.h"
 #include "BulletsManager.h"
 #include "Messages.h"
+#include "Observable.h"
 
 class GunInputComponent : public InputComponent, public Observable{
 private:
 	SDL_Keycode shoot_;
 	BulletsManager* bulletsManager_;
 public:
-	GunInputComponent(BulletsManager* bulletsManager, SDL_Keycode shoot) : shoot_(shoot) { bulletsManager_ = bulletsManager; }
+	GunInputComponent(BulletsManager* bulletsManager, SDL_Keycode shoot) : shoot_(shoot), bulletsManager_(bulletsManager) {}
 	~GunInputComponent(){}
 	virtual void handleInput(GameObject* o, Uint32 time, const SDL_Event& event);
 };
